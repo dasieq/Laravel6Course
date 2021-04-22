73215f0e1a0eac604a22859dccc3bdf0b18d3a08
AdamPytlik <adam.pytlik@post.cz>
2021-04-22 20:26:18 +0200

    finished the 8th lesson

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="flex-center position-ref full-height">
     <div class="content">
+        <img src="/img/pizza-house.png" alt="pizza house logo">
         <div class="title m-b-md">
             Herzlich Wilkommen!!!
         </div>
