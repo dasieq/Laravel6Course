ed3e50941cc5a26107f2cde82ff0735de5efb8cc
AdamPytlik <adam.pytlik@post.cz>
2021-04-25 22:52:04 +0200

    finished lesson-22

--- resources/views/pizzas/show.blade.php
+++ resources/views/pizzas/show.blade.php
@@ -12,6 +12,12 @@
     <li>{{$topping}}</li>
     @endforeach
     </ul>
+    <form action="/pizzas/{{$pizza->id}}" method="post">
+    @csrf
+    @method('DELETE') 
+    <button>Complete Order</button>
+    
+    </form>
     </div>
 <a href="/pizzas" class="back"><- Back to all pizzas</a>
 @endsection
